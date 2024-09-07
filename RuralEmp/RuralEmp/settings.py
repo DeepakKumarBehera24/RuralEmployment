b9f940de931dc51d5c9dcd8ead5e0fbec4783396
Service-Provider <deepakjgrt99@gmail.com>
2024-09-07 13:24:55 +0530

    added requirements.txt file to the project folder

--- RuralEmp/RuralEmp/settings.py
+++ RuralEmp/RuralEmp/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
+# Static asset configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
